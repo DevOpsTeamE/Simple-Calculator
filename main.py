dfa5a404373d348cefff080aca81b9c0eb9f04a3
xinenlim <S10221987@connect.np.edu.sg>
2023-10-28 00:57:24 +0800

    convert percentage function

--- main.py
+++ main.py
@@ -29,6 +29,27 @@ def perform_exponents():
 
     print(f"Result: {result:.2f}\n") 
 
+def convert_to_percentage():
+    while True:
+        try:
+            # Get user input as a decimal number
+            number = float(input("Enter a decimal number: "))
+            
+            # Check if the input is in the valid range (0 to 1)
+            if 0 <= number <= 1:
+                # Convert the number to a percentage
+                percentage = number * 100
+                
+                # Print the result
+                print(f"{number} as a percentage is: {percentage:.2f}%")
+                break
+            else:
+                print("Please enter a number between 0 and 1.")
+        except ValueError:
+            print("Invalid input. Please enter a valid decimal number.")
+
+
+
 while True:
     mainMenu();
     selection = float(input("Input your choice: "))
